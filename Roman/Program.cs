5125efce6a35eca2dc772b4c43d308f211237016
Administrator <zdvitas@gmail.com>
2014-12-08 02:07:30 +0300

    Final fix

--- Roman/Program.cs
+++ Roman/Program.cs
@@ -21,181 +21,18 @@ namespace Roman
 	class MainClass
 	{
 
-		public static string mas_to_string(string[] mas)
-		{
-			// 0	1	  2	  3 4  5  6  7    8    9       10    11    12   13   14   15   16   17   18
-			// N    Nver Год  М Д Час М Сек  Mg   Lon      Lat  Depth  Ver  AzP  PlP  AzT PlT  str   NAME
-			string delimeter = "\t";
-			string rez = "";
-			// Убираем 2 лишниее колонки и записываем дату и время
-			for (int i = 2; i < 8; i++) {
-				rez += mas [i] + delimeter;
-			} 
-			//в) "MHL","E" это две одинаковые колонки в нашем списке это Mg
-			rez += mas[8] + delimeter;
-			rez += mas[8] + delimeter;
-			//г) "kl" у нас такой колонки нет, поэтому зафигачиваем ее единицами всю
-			rez += "1" + delimeter ;
-			//д) "Ld","FI"  = нашим "Lon","Lat"
-			rez += mas[9] + delimeter;
-			rez += mas[10] + delimeter;
-			// е) "Depth" = Depth
-			rez += mas[11] + delimeter;
-			// ж) "Pazm","Ppl","Tazm","Tpl" = нашим  AzP  PlP  AzT PlT
-			rez += mas[13] + delimeter;
-			rez += mas[14] + delimeter;
-			rez += mas[15] + delimeter;
-			rez += mas[16] + delimeter;
-			//з)  "D" = str 	и)  "N" = Name 	к)  "B" = Nver	л)  "C" = Ver
-			rez += mas[17] + delimeter;
-			rez += mas[18] + delimeter;
-			rez += mas[1] + delimeter;
-			rez += mas[12] + delimeter;
-			return rez;
-		}
-
-		public static void restrict_new_text()
-		{
-			string[] text = System.IO.File.ReadAllLines("new_text.txt");
-			List<string> new_text2 = new List<string> ();
-			for (int i = 0; i < text.Length; i++) {
-				string[] tmp = text [i].Split (' ');
-
-				if (tmp [2].Length == 1) {
-					tmp [2] = "190" + tmp [2];
-				} else
-					tmp [2] = "19" + tmp [2];
-
-				if (tmp [5] == "-1" || tmp [5] == "24") {
-					tmp [5] = "0"; 
-				}
-
-
-				if (tmp [6] == "-1") {
-					tmp [6] = "0"; 
-				}
-
-
-				if (tmp [7] == "-1") {
-					tmp [7] = "0"; 
-				}
-
-				new_text2.Add (mas_to_string (tmp));
-
-			}
-			System.IO.File.WriteAllLines("new_text2.txt",new_text2);
-		}
-
-		public static void make_new_text()
-		{
-//			string[] rang_1 = {"ave","ccc","chi","den","dze","hod"};
-//			string[] rang_2 = {"hrv","mcd","mgk","mos","nis","mostr"};
-//			string[] rang_3 = {"oth","riz","rud","sip","yng","sip","rudik1","rudik2"};
-
-			string[] rang_1 = { "hrv", "cmt", "dze", "mcd" };
-			string[] rang_2 = { "hod" }; 
-			string[] rang_3 = { "sip" };
-			string[] rang_4 = { "ccc" };
-			string[] rang_5 = { "mgk" };
-			string[] rang_6 = { "rudik1", "rudik2" };
-			string[] rang_7 = { "riz" };
-			string[] rang_8 = { "nis" };
-			string[] rang_9 = { "den", "ave" };
-			string[] rang_10 = { "yng" };
-			string[] rang_11 = { "mostr", "chi", "oth" };
-
-
-
-			List<string[]> rangs = new List<string[]>();
-			rangs.Add(rang_1);
-			rangs.Add(rang_2);
-			rangs.Add(rang_3);
-			rangs.Add(rang_4);
-			rangs.Add(rang_5);
-			rangs.Add(rang_6);
-			rangs.Add(rang_7);
-			rangs.Add(rang_8);
-			rangs.Add(rang_9);
-			rangs.Add(rang_10);
-			rangs.Add(rang_11);
-
-
-			string[] text = System.IO.File.ReadAllLines("CATALOG.COL");
-
-			for(int j = 0; j< text.Length; j++){
-				for(int i = 0; i < 5; i++)
-				{
-					text[j] = text[j].Replace("  "," " );
-				}
-				if(text[j][0] == ' ')
-					text[j] = text[j].Remove(0,1);
-			}
-
-
-			List<string> new_text = new List<string>();
-
-			new_text.Add(text[0]);
-
-			string cur_string="";
-			int max_rang = int.MaxValue;
-
-			//for(int i=1; i< 300; i++)
-			for(int i=1; i< text.Length; i++)
-			{
-				var tmp = text[i].Split(' ');
-				if(tmp.Length == 12)
-				{
-
-					new_text[new_text.Count-1] +=" " + cur_string ;
-					cur_string = "";
-					new_text.Add(text[i]);
-					max_rang = int.MaxValue;
-				} else
-				{
-					string status = tmp[6].Replace('_','-');
-					status = status.Split('-')[0];
-					status = status.Split('_')[0];
-					for(int j = 0; j < rangs.Count; j++)
-					{
-						if (Array.IndexOf(rangs[j] , status) >= 0)
-						{
-							if(j<max_rang)
-							{
-								cur_string =text[i];
-								cur_string.Remove(0,1);
-								max_rang = j;
-
-							}
-						}
-					}
-				}
-
-
-
-			} 
-			new_text[new_text.Count-1] +=" " + cur_string ;
-			System.IO.File.WriteAllLines("new_text.txt",new_text);
-		}
-
 		public static void Main (string[] args)
 		{
 			//make_new_text ();
 			Func();
 			//restrict_new_text ();
 
-
-
 		}
 
 
 		public static void Func(){
-
-
-
 			string[] TB_text = System.IO.File.ReadAllLines("TB.txt");
 			string[] Tibet_text = System.IO.File.ReadAllLines("Tibet1.txt");
-
-			List<string> str_rez = new List<string> ();
 
 			List<Event> Event_TB = new List<Event> ();
 			List<Event> Event_Tibet = new List<Event> ();
@@ -203,28 +40,9 @@ namespace Roman
 
 			List<string> rez = new List<string> ();
 		
-			// Prepare date
+			// Заносим ТВ в базу
 			for (int i = 0; i < TB_text.Length; i++) {
 				string[] tmp = TB_text [i].Split ('\t');
-				// Костыль на кривую дату
-				/*if (tmp  [2].Length == 1) {
-					tmp  [2] = "190" + tmp  [2];
-				} else
-					tmp  [2] = "19" + tmp  [2];
-
-				if (tmp  [5] == "-1" || tmp  [5]=="24") {
-					tmp  [5] = "0"; 
-				}
-
-				
-				if (tmp  [6] == "-1") {
-					tmp  [6] = "0"; 
-				}
-
-				
-				if (tmp  [7] == "-1") {
-					tmp [7] = "0"; 
-				}*/
 
 				// Преобразуем дату в читаьельный формат
 				string date = tmp  [3] + "/" + tmp  [2] + "/" + tmp  [1] + " " +
@@ -251,12 +69,10 @@ namespace Roman
 			}
 
 
-
-			//
+			// Заносим тибет в базу
 
 			for (int i = 0; i < Tibet_text.Length; i++) {
-				Tibet_text[i] = Tibet_text [i].Replace ('\t' ,' ');
-				var tmp = Tibet_text [i].Split (' ');
+				var tmp = Tibet_text [i].Split ('\t');
 
 
 				string date = tmp [3] + "/" + tmp [2] + "/" + tmp  [1] + " " +
@@ -275,8 +91,6 @@ namespace Roman
 			rez_event.AddRange (Event_TB);
 
 		
-			// Это тупое прихуячивание
-			//rez.AddRange (list2);
 
 			// Прихуячиваем второй список к первому
 			int count_collisons = 0;   /// Эта переменная считает количество замещений
@@ -287,9 +101,11 @@ namespace Roman
 				Console.WriteLine ("Progress : "+i.ToString() + "/" + Event_Tibet.Count.ToString());
 				Console.WriteLine ("Colision count : " + count_collisons.ToString());
 				flag = true;
+
+				// проверка событий
 				for (int j = 0; j < Event_TB.Count; j++) {
 					if (test_events (Event_TB[j] , Event_Tibet[i])) {
-						// Значит что события произошли в одно время
+						// Значит что события произошли в одно время с заданными парамтерами
 						index = j;    /// ИНдекс события из первого списка
 						flag = false;
 						break;
@@ -298,19 +114,23 @@ namespace Roman
 
 				}
 
-
+				// или заменяем или добавляем
 				if (flag) {
-					Event_Tibet [i].data = "*** \t" + Event_Tibet[i].data; // Вот это закоментить
+
+					// add
+					Event_Tibet [i].data = "+++\t" + Event_Tibet[i].data; // Вот это закоментить
 					rez_event.Add (Event_Tibet [i]);
-	
+			
 
 				} else {
 					// Сейчас будем заменять
 					count_collisons ++;
-					Console.WriteLine ("From: \t" + rez_event [index].data);
-					Console.WriteLine (" To : \t" + Event_Tibet [i].data); // Пишет в консоль на что заменяем
-					Console.WriteLine("--------------------------");
-					Thread.Sleep (1000);
+				//	Console.WriteLine ("From: \t" + rez_event [index].data);
+				//	Console.WriteLine (" To : \t" + Event_Tibet [i].data); // Пишет в консоль на что заменяем
+				//	Console.WriteLine("--------------------------");
+				//	Thread.Sleep (1000);
+					Event_Tibet [i].data = "***\t" + Event_Tibet[i].data; // Вот это закоментить
+
 					rez_event [index] = Event_Tibet [i];
 
 				}
@@ -321,7 +141,7 @@ namespace Roman
 
 			}
 
-
+			// Подготовка к печати в файл
 
 			// Сортировка по времени 
 			for (int i = 0; i < rez_event.Count; i ++)
@@ -348,18 +168,28 @@ namespace Roman
 		public static bool test_events(Event event1 , Event event2){
 
 			float dt_float = 120f;
+
 			var mag1 = float.Parse (event1.columns [7].Replace('.',','));
 			var mag2 = float.Parse (event2.columns [7].Replace('.',','));
+			var mag = Math.Abs(mag1 - mag2);
 			float d_mag = 0.5f;
+
 			var depth1 = float.Parse (event1.columns [12].Replace('.',','));
 			var depth2 = float.Parse (event2.columns [12].Replace('.',','));
+			var depth = Math.Abs( depth1 - depth2);
 			float d_depth = 20.0f;
-			var cord1_1 = float.Parse (event1.columns [11].Replace('.',','));
+
+
+			var cord1_1 = float.Parse (event1.columns [10].Replace('.',','));
 			var cord1_2 = float.Parse (event1.columns [11].Replace('.',','));
 
-			var cord2_1 = float.Parse (event1.columns [10].Replace('.',','));
-			var cord2_2 = float.Parse (event1.columns [10].Replace('.',','));
+			var cord2_1 = float.Parse (event2.columns [10].Replace('.',','));
+			var cord2_2 = float.Parse (event2.columns [11].Replace('.',','));
+
+			var cord1 = Math.Abs (cord1_1 - cord2_1);
+			var cord2 = Math.Abs (cord1_2 - cord2_2);
 			float d_cord = 0.2f;
+
 
 			TimeSpan dt;
 			//return false; // НИкогда не заменяет
@@ -368,13 +198,19 @@ namespace Roman
 			else
 				dt = event2.time - event1.time;
 
-			if (dt.TotalSeconds <= dt_float)
+			if (dt.TotalSeconds <= dt_float) {
 
-			if (Math.Abs (mag1 - mag2) < d_mag) // Магнитуда
-			if (Math.Abs (depth1 - depth2) < d_depth) // Гулибна
-			if ((Math.Abs (cord1_1 - cord2_1) < d_cord) && (Math.Abs (cord1_2 - cord2_2) < d_cord)) // Координаты
-				return true;
+				if (mag < d_mag) { // Магнитуда
 
+					if (depth < d_depth) { // Гулибна
+						if (cord1 < d_cord) {
+							if (cord2 < d_cord) { // Координаты
+								return true;
+							}
+						}
+					}
+				}
+			}
 			return false;
 		}
 
